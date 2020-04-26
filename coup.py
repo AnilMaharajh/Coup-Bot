a497af9ae8836b3e63f72f6a43ea0baf0cb9be08
DeamonReapers <anil.maharajh@outlook.com>
2020-04-26 01:58:43 -0400

    Made player actions into the Coup Class Added general implementation ideas for functions

--- coup.py
+++ coup.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Optional, Tuple
 import random
 
 
@@ -83,28 +83,6 @@ class Player:
         self.coin = 2
         self.cards = []
 
-    def income(self) -> None:
-        """
-        Player takes 1 coin
-        """
-        self.coin += 1
-
-    def foreign_aid(self) -> None:
-        """
-        Player takes 2 coins, can be blocked with a duke
-        """
-
-    def bluff(self, action: str) -> bool:
-        """
-        In a 30 second period, asks all players if they allow the player to do an action
-        """
-        # Players allow the action
-        # if player1 and player2 ...... all true
-
-        # else:
-        # if action == foreign_aid
-        #   block_aid(player commit action, player_block)
-
 
 def generate_deck() -> Stack:
     """
@@ -134,7 +112,7 @@ class Coup:
                  player5: Player = None, player6: Player = None):
         """
         Creates environment for the game to be played
-        Players are generated
+        Players are generated and given two influence
         Play order is set
         """
         if player2 is None:
@@ -148,22 +126,73 @@ class Coup:
         player2.cards = self.card_stack.pop2()
         players.append(player2)
         self.total_coins = 46
+        self.remaining_players = 2
 
         if player3 is not None:
             player3.cards = self.card_stack.pop2()
             players.append(player3)
             self.total_coins -= 2
+            self.remaining_players += 1
         if player4 is not None:
             player4.cards = self.card_stack.pop2()
             players.append(player4)
             self.total_coins -= 2
+            self.remaining_players += 1
         if player5 is not None:
             player5.cards = self.card_stack.pop2()
             players.append(player5)
             self.total_coins -= 2
+            self.remaining_players += 1
         if player6 is not None:
             player6.cards = self.card_stack.pop2()
             players.append(player6)
             self.total_coins -= 2
+            self.remaining_players += 1
 
-        random.shuffle(self.players)
+    def income(self, player: Player) -> None:
+        """
+        Player takes 1 coin
+        """
+        player.coin += 1
+
+    def foreign_aid(self, player: Player) -> None:
+        """
+        Player takes 2 coins, can be blocked with a duke
+        """
+        condition = self.player_check(player)
+        if condition is None:
+            player.coin += 2
+        else:
+            self.challenge(player, condition, "foreign")
+
+    def assassinate(self, player, b_player):
+        player.coin -= 3
+        condition = self.player_check(player)
+        if condition is None:
+
+
+    def player_check(self, player: Player) -> Optional[Player]:
+        """
+        Allow opposing players to block or bluff the player committing an action
+        If nobody responds after 30 seconds, player action goes through
+        """
+        # All the other players can bypass the 30 seconds by responding N
+        # If one player responds with Y, they challenge the player
+        return None
+
+    def challenge(self, player: Player, b_player: Player, action: str):
+        """
+        b_player the one who is challenging player to show their card if they are lying
+        Player can show their card, and prove that they have it. Thus, b_player must put away one of their influence
+        Player can show their card regardless of its influence and put it in the deck, or put it in the deck
+        """
+
+        # if player decides to show their correct card
+        #   Can either keep it, or put in the deck to exchange for the first card
+        #   b_player must give up one of their cards
+        #   checks if b_player has 0 cards, if so they are popped out of the list, eliminating them from the game
+
+        # or player decides to discard one of their cards
+        # checks if player has 0 cards, if so they are popped out of the list, eliminating them from the game
+
+
