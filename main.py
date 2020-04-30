7740adbacbac686cf784d9fdcf6344fffd5b4bda
DeamonReapers <anil.maharajh@outlook.com>
2020-04-30 12:39:28 -0400

    Started work on the discord implementation

--- main.py
+++ main.py
@@ -1,12 +1,97 @@
 import discord
 from discord.ext import commands
+from typing import Any, List, Optional
 import random
 
 client = commands.Bot(command_prefix='//')
 client.run("Njg2Mzk1ODU0NjU5MTI1MzQ1.XmWoXA.mzQOJaPytBGPMmu8x77PREFViOQ")
 
 
+class Stack:
+    """A last-in-first-out (LIFO) stack of items.
+
+    Stores data in a last-in, first-out order. When removing an item from the
+    stack, the most recently-added item is the one that is removed.
+    """
+    # === Private Attributes ===
+    # _items:
+    #     The items stored in this stack. The end of the list represents
+    #     the top of the stack.
+    _items: List
+
+    def __init__(self) -> None:
+        """Initialize a new empty stack."""
+        self._items = []
+
+    def is_empty(self) -> bool:
+        """Return whether this stack contains no items.
+
+        >>> s = Stack()
+        >>> s.is_empty()
+        True
+        >>> s.push('hello')
+        >>> s.is_empty()
+        False
+        """
+        return self._items == []
+
+    def push(self, item: Any) -> None:
+        """Add a new element to the top of this stack."""
+        self._items.append(item)
+
+    def push_bottom(self, item: Any) -> None:
+        """
+        Adds a element to the bottom of this stack
+        """
+        self._items.insert(0, item)
+
+    def pop(self) -> Any:
+        """Remove and return the element at the top of this stack.
+
+        Raise an EmptyStackError if this stack is empty.
+
+        >>> s = Stack()
+        >>> s.push('hello')
+        >>> s.push('goodbye')
+        >>> s.pop()
+        'goodbye'
+        """
+        if self.is_empty():
+            raise EmptyStackError
+        else:
+            return self._items.pop()
+
+    def pop2(self) -> Any:
+        """
+        Remove and return the element at the top of this stack.
+        Raise an EmptyStackError if this stack is empty.
+        """
+        if self.is_empty():
+            raise EmptyStackError
+        else:
+            return [self._items.pop(), self._items.pop()]
+
+
+class EmptyStackError(Exception):
+    """Exception raised when an error occurs."""
+    pass
+
+
 @client.event
 async def on_ready():
     # console message to confirm bot has logged in
     print('We have logged in as {0.user}'.format(client))
+
+
+@client.command
+async def coup(ctx, *players):
+    """
+    To add players to a game of coup, you must mention them
+    Total of 2-6 players can play the game
+    Any additional players are ignored
+    """
+    print("work")
+    for player in players:
+        print(player)
+
+    await ctx.send("Now creating coup game with: ")
